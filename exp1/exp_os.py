5c7c37bfc30b1c72119aaf025bd0cb77c6e13a6c
Dreamfantcy <yks318@163.com>
2015-06-06 02:17:01 +0800

    20150605 Changes log

---
+++ exp1/exp_os.py
@@ -0,0 +1,6 @@
+# -*- coding: cp936 -*-
+import os
+os.chdir("E:\mPython\PythonExp\exp1")
+print "Operate System Name: ",os.name;
+print "Current Work Directory:",str(os.getcwd())
+print "Current Directories and Files List:",os.listdir(os.getcwd())
